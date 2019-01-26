8356e721fc73195a511eb7094bc35e5e94b7696e
Steven Sheffey <srs6p@mtmail.mtsu.edu>
2019-01-26 17:14:21 -0600

    GAMERS RISE UP

--- Assets/scripts/AttackHandler.cs
+++ Assets/scripts/AttackHandler.cs
@@ -13,6 +13,9 @@ public class AttackHandler : MonoBehaviour
     private bool p1Ready;
     private bool p2Ready;
 
+    private List<NoteType> p1Notes;
+    private List<NoteType> p2Notes;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +25,9 @@ public class AttackHandler : MonoBehaviour
         p2Ready = false;
         p1Offset = 0.0;
         p2Offset = 0.0;
+
+        p1Notes = new List<NoteType>();
+        p2Notes = new List<NoteType>();
     }
 
     // Update is called once per frame
@@ -31,10 +37,92 @@ public class AttackHandler : MonoBehaviour
     }
     void Beat(double beatNum)
     {
-        GetComponent<TextMesh>().text = BeatInput.AttackToArrows(p1Attack) + BeatInput.AttackToArrows(p2Attack) + "\n" + Mathf.Round(1000F*(float)p1Offset).ToString() + " " + Mathf.Round(1000F * (float)p2Offset).ToString();
+        // Aggregate note types
+        char[] p1ActualC = "    ".ToCharArray();
+        foreach(NoteType note in p1Notes)
+        {
+            switch(note)
+            {
+                case NoteType.left:
+                    p1ActualC[0] = '<';
+                    break;
+                case NoteType.up:
+                    p1ActualC[1] = '^';
+                    break;
+                case NoteType.down:
+                    p1ActualC[2] = 'v';
+                    break;
+                case NoteType.right:
+                    p1ActualC[3] = '>';
+                    break;
+                default:
+                    break;
+            }
+        }
+        string p1Actual = new string(p1ActualC);
+        // Aggregate note types
+        char[] p2ActualC = "    ".ToCharArray();
+        foreach (NoteType note in p2Notes)
+        {
+            switch (note)
+            {
+                case NoteType.left:
+                    p2ActualC[0] = '<';
+                    break;
+                case NoteType.up:
+                    p2ActualC[1] = '^';
+                    break;
+                case NoteType.down:
+                    p2ActualC[2] = 'v';
+                    break;
+                case NoteType.right:
+                    p2ActualC[3] = '>';
+                    break;
+                default:
+                    break;
+            }
+        }
+        string p2Actual = new string(p2ActualC);
+
+        string p1Attempted = BeatInput.AttackToArrows(p1Attack);
+        string p2Attempted = BeatInput.AttackToArrows(p2Attack);
+
+        string p1Message;
+        if (p1Actual == "    ")
+        {
+            p1Message = "   ";
+        }
+        else if (p1Attempted == p1Actual)
+        {
+            p1Message = "HIT";
+        }
+        else
+        {
+            p1Message = "MISS";
+        }
+        string p2Message;
+        if(p2Actual == "    ")
+        {
+            p2Message = "   ";
+        }
+        else if (p2Attempted == p2Actual)
+        {
+            p2Message = "HIT";
+        }
+        else
+        {
+            p2Message = "MISS";
+        }
+
+        // GetComponent<TextMesh>().text = p1Attempted + p2Attempted + "\n" + p1Actual + p2Actual + "\n" + Mathf.Round(1000F*(float)p1Offset).ToString() + " " + Mathf.Round(1000F * (float)p2Offset).ToString();
+        GetComponent<TextMesh>().text = p1Message + "   " + p2Message;
         GetComponent<TextMesh>().text.Replace("\n", "\\n");
+
         p1Attack = BeatInput.Attack.None;
         p2Attack = BeatInput.Attack.None;
+        p1Notes.Clear();
+        p2Notes.Clear();
+        
     }
     public void BeatReady(Player player)
     {
@@ -103,6 +191,19 @@ public class AttackHandler : MonoBehaviour
         }
     }
 
+    public void AddNote(Player player, NoteType noteType)
+    {
+        switch (player)
+        {
+            case Player.Player1:
+                p1Notes.Add(noteType);
+                break;
+            case Player.Player2:
+                p2Notes.Add(noteType);
+                break;
+        }
+        
+    }
     public enum Player
     {
         Player1,
